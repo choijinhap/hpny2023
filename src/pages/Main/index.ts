0cd5b9047bcb0c3bbcb7ab4da639ebd3fadf00f0
choijinhap <wlsgkq123@gmail.com>
2023-01-12 10:43:44 +0900

    feat : 메인페이지 템플릿 작성

---
+++ src/pages/Main/index.ts
@@ -0,0 +1,35 @@
+import Component from 'src/lib/Component';
+import './main.scss';
+
+class Main extends Component<null> {
+	template() {
+		return `
+      <div class="Main">
+        <button class="create-post-btn">
+          새 글 작성하기
+        </button>
+        <div class="post-list">
+          <div class="post-item">
+            <div class="img-wrap">
+              <img src="https://source.unsplash.com/random/360×360"/>
+            </div>
+            <div class="description">
+              <strong>신년 계획</strong>
+              <p>다들 신년 계획 세우셨나요?</p>
+            </div>
+          </div>
+          <div class="post-item">
+            <div class="img-wrap">
+              <img src="https://source.unsplash.com/random/360×360"/>
+            </div>
+            <div class="description">
+              <strong>신년 계획</strong>
+              <p>다들 신년 계획 세우셨나요?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+	}
+}
+export default Main;
