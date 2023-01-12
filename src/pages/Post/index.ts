1c7f5385cc202d28d31d56f72447b0e2846755a7
choijinhap <wlsgkq123@gmail.com>
2023-01-12 10:56:32 +0900

    feat : 글작성페이지 템플릿 작성

---
+++ src/pages/Post/index.ts
@@ -0,0 +1,20 @@
+import Component from 'src/lib/Component';
+import './post.scss';
+
+class Post extends Component<null> {
+	template() {
+		return `
+      <div class="Post">
+        <button class="create-post-btn active">
+          랜덤 이미지 추가하기
+        </button>
+        <input class="title" type="text"/>
+        <input class="content" type="text"/>
+        <button class="create-post-btn">
+          글 작성하기
+        </button>
+      </div>
+    `;
+	}
+}
+export default Post;
