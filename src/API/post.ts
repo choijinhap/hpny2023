a1ec36fa1da965223ac970c96b9f0ccb51a651d5
choijinhap <wlsgkq123@gmail.com>
2023-01-14 16:52:09 +0900

    feat : 게시글 삭제 기능 구현

--- src/API/post.ts
+++ src/API/post.ts
@@ -4,3 +4,4 @@ export const getAllPosts = () => server.get('/posts');
 export const postPost = ({ title, content, image }: Partial<Post>) =>
 	server.post('/post', { title, content, image });
 export const getPostDetail = (postId: string) => server.get(`/post/${postId}`);
+export const deletePost = (postId: string) => server.delete(`/post/${postId}`);
--- src/components/Detail/PostDetail/index.ts
+++ src/components/Detail/PostDetail/index.ts
@@ -1,5 +1,7 @@
 import { Post } from 'src/@types/post';
+import { deletePost } from 'src/API/post';
 import Component from 'src/lib/Component';
+import { navigate } from 'src/lib/Router';
 import { IsoToDate } from 'src/utils/time';
 import './postdetail.scss';
 
@@ -9,7 +11,6 @@ type Props = {
 
 class PostDetail extends Component<null, Props> {
 	template(): string {
-		console.log(this.props.post);
 		const { image, title, createdAt, content } = this.props.post;
 		return `
         <div class="post-detail">
@@ -20,11 +21,23 @@ class PostDetail extends Component<null, Props> {
             <small>${IsoToDate(createdAt)}</small>
             <p>${content}</p>
             <div class="post-btn-wrapper">
-              <button class="post-modify=btn">수정</button>
+              <button class="post-modify-btn">수정</button>
               <button class="post-delete-btn">삭제</button>
             </div>
           </div>
       `;
+	}
+	setEvent(): void {
+		this.addEvent('click', '.post-delete-btn', this.deletePostHandler.bind(this));
+	}
+
+	async deletePostHandler() {
+		const res = await deletePost(this.props.post.postId);
+		if (res.data.code === 200) {
+			navigate('/');
+			return;
+		}
+		alert('삭제중 에러가 발생했습니다.');
 	}
 }
 export default PostDetail;
